198a63f472e1391ec15e540febf5a9508fa40d28
HBhawe <harshalb97@outlook.com>
2024-07-10 11:00:31 +0200

    Completed exercise 2

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  //   SECOND HAND
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  //   MINUTE HAND
+  const minutes = now.getMinutes();
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  //   HOUR HAND
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+// sets an interval every second (1000 ms)
+setInterval(setDate, 1000);
